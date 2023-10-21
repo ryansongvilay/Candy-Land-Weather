ae7ec1a82a52edf3f92e378b12b47f09849d5beb
Joshua Yu <joshyu@utexas.edu>
2023-10-21 15:26:29 -0500

    testing

--- src/api.js
+++ src/api.js
@@ -3,7 +3,9 @@ export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'YOUR API KEY HERE',
+		'X-RapidAPI-Key': '589ef71616msh01b93f52f6e4bbfp1789f2jsnf962d963975c',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
+
+console.log("THIS SHOULD NOT BE ON GITHUB");
