8db75ec6224f24a43e2e64460441d015366fbfc2
Joshua Yu <joshyu@utexas.edu>
2023-10-21 15:24:13 -0500

    blank api key

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'afd896ee37msh8a5de21648a3bddp1e883ejsn6fdceb3dbb4c',
+		'X-RapidAPI-Key': 'YOUR API KEY HERE',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
