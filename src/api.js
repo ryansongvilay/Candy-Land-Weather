829fc84293602d409b848bfc10bbbdf8187a3429
Joshua Yu <joshyu@utexas.edu>
2023-10-21 15:37:57 -0500

    api key security

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
+import API_KEY from "./apiKey";
+
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'YOUR API KEY HERE',
+		'X-RapidAPI-Key': API_KEY,
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
-
-console.log("im testing");
