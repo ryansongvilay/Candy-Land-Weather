d47ff2288542a0f412ffd81d21b69d52b7ab043b
Joshua Yu <joshyu@utexas.edu>
2023-10-21 19:03:41 -0500

    full skeleton

--- src/components/screens/Weather.js
+++ src/components/screens/Weather.js
@@ -4,9 +4,6 @@ import { useNavigate } from "react-router-dom";
 const Weather = ({ data, handleOnSearchChange }) => {
   const navigate = useNavigate();
 
-  const temperature = Math.round((data.main.temp - 273.15) * (9 / 5) + 32);
-  const city = data.city.split(",")[0];
-
   const onClick = () => {
     navigate("/");
   };
@@ -18,7 +15,7 @@ const Weather = ({ data, handleOnSearchChange }) => {
       </div>
       {data && (
         <div>
-          <p>City: {city}</p>
+          <p>City: {data.city.split(",")[0]}</p>
           <p>Wind speed: {data.wind.speed} m/s</p>
           <p>Humidity: {data.main.humidity}%</p>
           <p>
@@ -28,13 +25,52 @@ const Weather = ({ data, handleOnSearchChange }) => {
           <p>Forecast: {data.weather[0].description}</p>
           <p>
             {/* kelvin to fahrenheit */}
-            Temperature: {temperature} °F
+            Temperature: {Math.round(
+              (data.main.temp - 273.15) * (9 / 5) + 32
+            )}{" "}
+            °F
           </p>
           <p>AQI: {data.aqi}</p>
-          <p>
-            Gingerbread man says:
-            {temperature > 100 &&
-              `Woah, ${city} is baking today! Remember to stay hydrated with a refreshing candy juice. Avoid melting out there!`}
+          <p className="weather-text">
+            Gingerbread man says:{" "}
+            {Math.round((data.main.temp - 273.15) * (9 / 5) + 32) > 100 &&
+              `Woah, ${
+                data.city.split(",")[0]
+              } is baking today! Remember to stay hydrated with a refreshing candy juice. Avoid melting out there!`}
+            {Math.round((data.main.temp - 273.15) * (9 / 5) + 32) <= 100 &&
+              Math.round((data.main.temp - 273.15) * (9 / 5) + 32) > 85 &&
+              `It’s warming up in ${
+                data.city.split(",")[0]
+              }! Perfect weather to bask under the gumdrop trees. Don’t forget your sun hat!`}
+            {Math.round((data.main.temp - 273.15) * (9 / 5) + 32) <= 85 &&
+              Math.round((data.main.temp - 273.15) * (9 / 5) + 32) > 65 &&
+              `A pleasant day in ${
+                data.city.split(",")[0]
+              } with mild temperatures. A great day for a stroll along the candy cane lane!`}
+            {Math.round((data.main.temp - 273.15) * (9 / 5) + 32) <= 65 &&
+              Math.round((data.main.temp - 273.15) * (9 / 5) + 32) > 32 &&
+              `It’s a chilly day in ${
+                data.city.split(",")[0]
+              }, with temperatures that might have you reaching for warm cocoa. Stay cozy!`}
+            {Math.round((data.main.temp - 273.15) * (9 / 5) + 32) <= 32 &&
+              `Brrr! ${
+                data.city.split(",")[0]
+              } is frosting over here with a temperature below freezing. Bundle up in your sweetest scarf and mittens!`}
+            {data.weather.main &&
+              data.weather.main.toLowerCase().includes("rain") &&
+              "However, expect some syrupy rain. If you’re venturing out, don’t forget your candy cane umbrella!"}
+            {data.weather.main &&
+              data.weather.main.toLowerCase().includes("storm") &&
+              "But watch out! There’s a storm brewing. Best to find shelter and wait it out."}
+            {data.weather.main &&
+              data.weather.main.toLowerCase().includes("hail") &&
+              "And be cautious! The sky is dropping icy surprises with a hailstorm. Better to stay indoors."}
+            {data.weather.main &&
+              data.weather.main.toLowerCase().includes("clear") &&
+              "And the skies? As clear as a freshly unwrapped lollipop. Enjoy."}
+            {data.weather.main &&
+              data.weather.main.toLowerCase().includes("fog") &&
+              "But visibility is a tad sticky with thick fog. Travel with caution on those candy roads."}
           </p>
         </div>
       )}
